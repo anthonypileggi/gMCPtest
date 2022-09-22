944d32cd78929eb38a393733779924c0a7cba6fd
Anthony Pileggi <apileggi20@gmail.com>
2022-09-22 15:16:03 -0400

    add checks for inputs

--- R/generate_weights.R
+++ R/generate_weights.R
@@ -3,8 +3,22 @@
 #' @param w A numeric vector of weights
 #' @export
 generate_weights <- function(G, w) {
+
+  # check inputs
+  if (nrow(G) != ncol(G))
+    stop("`G` must be a square transition matrix.")
+  if (nrow(G) != length(w))
+    stop("`w` and `G` must be of compatible lengths.")
+  if (sum(w) > 1)
+    stop("Weights must not sum to > 1.")
+  if (any(w < 0) | any(w > 1))
+    stop("All weights must be in range: [0, 1].")
+
+  # compute all permutations of the set (i.e. binary vectors)
   n <- length(w)
   intersect <- (permutations(n))[-1,]
+
+  # compute updated weights for each permutation
   purrr::map_df(
     1:nrow(intersect),
     function(i) {
@@ -15,4 +29,5 @@ generate_weights <- function(G, w) {
       )
     }
   )
+
 }
