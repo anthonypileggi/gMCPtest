b83d062ad5a4a56dc7750790a7cfa7c4ae1357a6
Anthony Pileggi <apileggi20@gmail.com>
2022-09-22 14:45:40 -0400

    use G instead of g for transition matrix

--- R/generate_weights.R
+++ R/generate_weights.R
@@ -1,8 +1,8 @@
 #' Compute all intersection hypotheses and corresponding weights for a given graph
-#' @param g Graph represented as transition matrix
+#' @param G Graph represented as transition matrix
 #' @param w A numeric vector of weights
 #' @export
-generate_weights <- function(g, w) {
+generate_weights <- function(G, w) {
   n <- length(w)
   intersect <- (permutations(n))[-1,]
   purrr::map_df(
@@ -11,7 +11,7 @@ generate_weights <- function(g, w) {
       h <- intersect[i, ]
       dplyr::tibble(
         set = list(h),
-        weights = list(update_weights(h, g, w))
+        weights = list(update_weights(h, G, w))
       )
     }
   )
--- R/update_weights.R
+++ R/update_weights.R
@@ -1,17 +1,17 @@
 #' Recursively compute weights for a given graph and intersection hypothesis
 #' @param h A numeric vector with only binary entries (0,1).
-#' @param g Graph represented as transition matrix.
+#' @param G Graph represented as transition matrix.
 #' @param w A numeric vector of weights.
 #' @return A weight vector
-update_weights <- function(h,g,w){
+update_weights <- function(h, G, w){
 
   if(sum(h) == length(h)){
     return(w)
   } else {
     j <- which(h == 0)[1]
     h[j] <- 1
-    wu <- update_weights(h, g, w)
-    gu <- update_edges(h,g,w)
+    wu <- update_weights(h, G, w)
+    gu <- update_edges(h, G, w)
     guj <- gu[j, ]
     wt <- wu + (wu[j] * guj)
     wt[j] <- 0
